b698b71f503f6366919e9030c104630ccf82d5c6
juandahl <juandahl20@gmail.com>
2018-11-12 20:50:28 +0100

    adding files

---
+++ src/mouvement.c
@@ -0,0 +1,125 @@
+//contiendra l’ensemble des algorithmes scalaires de détection de mouvement (ainsi que leur différentes implantations optimisées).
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "nrdef.h"
+#include "nrutil.h"
+
+#define VMIN 1
+#define VMAX 254
+
+#define Efond 0
+#define Emouv 255
+
+#define N 2 //N vaut 2 ou 3, éventuellement 4.
+
+long nrl, nrh, ncl, nch;
+
+void routine_FrameDifference(char* image1, char* image2, int threshold)
+{
+    //matrice de taille [nrl..nrh][ncl..nch]
+    long nrl, nrh, ncl, nch;
+    //chargement de 2 images
+    uint8 **I0 =  LoadPGM_ui8matrix(image1, &nrl, &nrh, &ncl, &nch);
+    uint8 **I1 =  LoadPGM_ui8matrix(image2, &nrl, &nrh, &ncl, &nch);
+
+
+    uint8 **E0 =  ui8matrix(nrl, nrh, ncl, nch);
+    uint8 **O0 = ui8matrix(nrl, nrh, ncl, nch);
+    for(int i = nrl; i < nrh; i++ )
+    {
+        for(int j = ncl; j < nch; j++)
+        {
+            O0[i][j] = abs(I1[i][j] - I0[i][j]);
+
+        }
+    }
+    
+    for(int i = nrl; i < nrh; i++ )
+    {
+        for(int j = ncl; j < nch; j++)
+        {
+            if(O0[i][j] < threshold)
+                E0[i][j] = Efond; //pas de mouvement
+            else
+                E0[i][j] = Emouv; //si mouvement
+
+        }
+    }
+
+    //generation d'une nouvelle image
+    SavePGM_ui8matrix(E0, nrl, nrh, ncl, nch, "frame_difference.pgm");
+    free_ui8matrix(I0, nrl, nrh, ncl, nch);
+    free_ui8matrix(I1, nrl, nrh, ncl, nch);
+    free_ui8matrix(E0, nrl, nrh, ncl, nch);
+    free_ui8matrix(O0, nrl, nrh, ncl, nch);
+}
+
+int maxmin(uint8 ** Vt)
+{
+    int result = Vt;
+    if (Vt > VMAX)
+        result = VMAX;
+    if (result > VMIN)
+        return result;
+    else
+        return VMIN;
+}
+
+void routine_SigmaDelta_step0_inicialisation(uint8** It1, uint8 **M, uint8 **V, long rawl, long rawh, long coll, long colh )
+{
+
+
+    nrl = rawl;
+    nrh = rawh;
+    ncl = coll;
+    nch = colh;    
+
+    for(int i = nrl; i <= nrh; i++ )
+        for(int j = ncl; j <= nch; j++)
+        {
+            M[i][j] = It1[i][j];
+            V[i][j] = VMIN;
+
+        }
+}
+
+//Mt = moyenne, vt= variance, Mt1 = Mt-1, Et = result  
+void routine_SigmaDelta_step1(uint8** It, uint8** Mt1, uint8** Mt, uint8** Vt, uint8** Vt1, uint8** Et){
+    //Verifier parameters et var locales
+    uint8 Ot;
+	//steps from 1 to 4 together to avoid repeat the loop
+    for(int i = nrl; i <= nrh; i++ )
+        for(int j = ncl; j <= nch; j++){
+        	//step 1
+        	if (Mt1[i][j] < It[i][j])
+        		Mt[i][j] = Mt1[i][j] + 1;
+        	else
+	        	if (Mt1[i][j] > It[i][j])
+	        		Mt[i][j] = Mt1[i][j] - 1;
+	        	else
+	        		Mt[i][j] = Mt1[i][j];
+	        
+	        //step 2
+            Ot = abs(Mt - It);
+         	
+         	//step 3
+         	if ( Vt1 < (int)N*Ot )
+         		Vt = Vt1 + 1;
+         	else
+	         	if ( Vt1 > (int)N*Ot )
+	         		Vt = Vt1 - 1;
+	            else
+	                Vt = Vt1;
+            Vt = maxmin(Vt);
+
+            //step 4
+            if(Ot < Vt)
+                Et[i][j] = Efond;
+            else
+                Et[i][j] = Emouv;
+        }
+}
---
+++ src/test_mouvement.c
@@ -0,0 +1,92 @@
+#define NUMBER_IMAGES 299
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "nrdef.h"
+#include "nrutil.h"
+#include "mymacro.h"
+
+void test_routine_sigmaDelta()
+{
+    puts("-----------------------------");
+    puts("-- test_routine_sigmaDelta --");
+    puts("-----------------------------");
+
+    //Inicialization variables
+    char imagePath[50];
+    char outputPath[50];
+    long nrl, nrh, ncl, nch; // variables needed to load images
+    long numCycles, totalCycles; //cycles counters 
+    char initialImagePath[50] = "hall/hall000000.pgm";
+    
+    //inicial matrix(step 0)
+    uint8** It1 = LoadPGM_ui8matrix(initialImagePath, &nrl, &nrh, &ncl, &nch);
+
+    uint8 **Mt1 =  ui8matrix(nrl, nrh, ncl, nch);
+    uint8 **Vt1 =  ui8matrix(nrl, nrh, ncl, nch);
+
+    //initial matrix (step1)
+    uint8 **It = ui8matrix(nrl, nrh, ncl, nch);
+    uint8 **Et = ui8matrix(nrl, nrh, ncl, nch);
+    uint8 **Mt =  ui8matrix(nrl, nrh, ncl, nch);
+    uint8 **Vt =  ui8matrix(nrl, nrh, ncl, nch);
+
+    // ------------ //
+    // -- calcul -- //
+    // ------------ //
+
+    routine_SigmaDelta_step0_inicialisation(It1, Mt1, Vt1, nrl, nrh, ncl, nch );
+    printf("%s%s\n","valor: ", imagePath);
+
+    for(int i = 1; i <= NUMBER_IMAGES; i++)
+    {
+
+        sprintf(imagePath, "hall/hall000%03d.pgm", i );
+
+        It = LoadPGM_ui8matrix(imagePath, &nrl, &nrh, &ncl, &nch);
+
+//        MLoadPGM_ui8matrix(imagePath, nrl, nrh, ncl, nch, It); //Load the new image
+
+        
+        CHRONO(routine_SigmaDelta_step1(It, Mt1, Mt, Vt, Vt1, Et), numCycles);
+        totalCycles = totalCycles + numCycles;
+        
+        sprintf(outputPath,"SigmaDelta/hall000%03d.pgm",i);
+        SavePGM_ui8matrix(Et, nrl, nrh, ncl, nch, outputPath);
+        
+        //update state of matrix
+        copy_ui8matrix_ui8matrix(Mt, nrl, nrh, ncl, nch, Mt1);
+        copy_ui8matrix_ui8matrix(Vt, nrl, nrh, ncl, nch, Vt1);
+        copy_ui8matrix_ui8matrix(It, nrl, nrh, ncl, nch, It1);
+    }
+
+    // -------------- //
+    // -- Results -- //
+    // ------------- //
+    
+    BENCH(printf("Cycles Total Sigma Delta = "));
+    BENCH(printf("%6.2f\n", totalCycles));
+
+    totalCycles = totalCycles / NUMBER_IMAGES;
+    BENCH(printf("Cycles per image Sigma Delta = "));
+    BENCH(printf("%6.2f\n", totalCycles));
+
+    totalCycles = totalCycles / ((nch+1)*(nrh+1));
+    BENCH(printf("Cycles per pixel Sigma Delta = "));
+    BENCH(printf("%6.2f\n", totalCycles));
+
+
+    // ---------- //
+    // -- free -- //
+    // ---------- //
+    free_ui8matrix(It1, nrl, nrh, ncl, nch );
+    free_ui8matrix(Mt1, nrl, nrh, ncl, nch );
+    free_ui8matrix(Vt1, nrl, nrh, ncl, nch );
+
+    free_ui8matrix(It, nrl, nrh, ncl, nch );
+    free_ui8matrix(Mt, nrl, nrh, ncl, nch );
+    free_ui8matrix(Vt, nrl, nrh, ncl, nch );
+    free_ui8matrix(Et, nrl, nrh, ncl, nch );
+
+}
+
